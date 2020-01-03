7cfba3240c5f2a890e48b7e7acd8d08a3cb551e4
Lucas Pinheiro <lucas_pinheiro.silva@hotmail.com>
2020-01-03 15:35:56 -0300

    ORGANIZZE CLONE

--- ANDROID/Organizze/app/src/main/java/br/com/organizze/activity/activity/PrincialActivity.java
+++ ANDROID/Organizze/app/src/main/java/br/com/organizze/activity/activity/PrincialActivity.java
@@ -21,14 +21,19 @@ public class PrincialActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+    public void adicionarReceita(View view){
+
+    }
+    public void adicionarDespesa(View view){
+
     }
 
 }
