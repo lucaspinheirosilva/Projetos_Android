bd12ad0bf43ae82b12665a4bc73880d0425e7566
Lucas Pinheiro <lucas_pinheiro.silva@hotmail.com>
2020-03-31 17:30:42 -0300

    Flutter

---
+++ FLUTTER/conversorMoedas/conversor_moedas/android/app/src/main/kotlin/pinheiro/com/conversor_moedas/MainActivity.kt
@@ -0,0 +1,6 @@
+package pinheiro.com.conversor_moedas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- FLUTTER/conversorMoedas/conversor_moedas/lib/main.dart
+++ FLUTTER/conversorMoedas/conversor_moedas/lib/main.dart
@@ -31,8 +31,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  double dollar;
+  double dolar;
   double euro;
+
+  final realController = new TextEditingController();
+  final dolarController = new TextEditingController();
+  final euroController = new TextEditingController();
+
+  void _realMudanca(String texto) {
+    double real = double.parse(texto);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
+  void _dolarMudanca(String texto) {
+    double dolar = double.parse(texto);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+  void _euroMudanca(String texto) {
+    double euro = double.parse(texto);
+    realController.text=(euro * this.euro).toStringAsFixed(2);
+    dolarController.text=(euro * this.euro / dolar).toStringAsFixed(2);
+  }
+  void ResetarCampos(){
+    realController.text="";
+    dolarController.text="";
+    euroController.text="";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +67,10 @@ class _HomeState extends State<Home> {
         title: Text("\$ Conversor \$"),
         backgroundColor: Colors.amber,
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(icon: Icon( Icons.refresh),
+              onPressed: ResetarCampos)
+        ],
       ),
       body: FutureBuilder<Map>(
           future: PegarDados(),
@@ -64,7 +93,7 @@ class _HomeState extends State<Home> {
                     textAlign: TextAlign.center,
                   ));
                 } else {
-                  dollar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
                   euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
 
                   return SingleChildScrollView(
@@ -77,32 +106,14 @@ class _HomeState extends State<Home> {
                           size: 150,
                           color: Colors.amber,
                         ),
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Reais",
-                              labelStyle: TextStyle(color: Colors.amber),
-                              border: OutlineInputBorder(),
-                              prefixText: "R\$ "),
-                          style: TextStyle(color: Colors.amber, fontSize: 25),
-                        ),
+                        criarTextField(
+                            "Real", "RS ", realController, _realMudanca),
                         Divider(),
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Dólares",
-                              labelStyle: TextStyle(color: Colors.amber),
-                              border: OutlineInputBorder(),
-                              prefixText: "US\$ "),
-                          style: TextStyle(color: Colors.amber, fontSize: 25),
-                        ),
+                        criarTextField(
+                            "Dollar", "US\$ ", dolarController, _dolarMudanca),
                         Divider(),
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Euros",
-                              labelStyle: TextStyle(color: Colors.amber),
-                              border: OutlineInputBorder(),
-                              prefixText: "€ "),
-                          style: TextStyle(color: Colors.amber, fontSize: 25),
-                        )
+                        criarTextField(
+                            "Euro", "€ ", euroController, _euroMudanca),
                       ],
                     ),
                   );
@@ -116,4 +127,19 @@ class _HomeState extends State<Home> {
 Future<Map> PegarDados() async {
   http.Response response = await http.get(requisicao);
   return json.decode(response.body);
+}
+
+Widget criarTextField(String labelText, String prefixText,
+    TextEditingController controlador, Function mudanca) {
+  return TextField(
+    controller: controlador,
+    decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefixText),
+    style: TextStyle(color: Colors.amber, fontSize: 25),
+    onChanged: mudanca,
+    keyboardType: TextInputType.number,
+  );
 }
