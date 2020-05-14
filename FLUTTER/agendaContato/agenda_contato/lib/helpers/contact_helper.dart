3dff9d306f9b875b933a1db01791379d3230e5e7
Lucas Pinheiro <lucas_pinheiro.silva@hotmail.com>
2020-05-14 17:57:52 -0300

    Lista de Contato

--- FLUTTER/agendaContato/agenda_contato/lib/helpers/contact_helper.dart
+++ FLUTTER/agendaContato/agenda_contato/lib/helpers/contact_helper.dart
@@ -1,10 +1,60 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+final String contactTable = "contactTable";
 final String idColuna = "idColuna";
 final String nomeColuna = "nomeColuna";
 final String emailColuna = "emailColuna";
 final String foneColuna = "foneColuna";
 final String imgColuna = "imgColuna";
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final dataBasePath = await getDatabasesPath();
+    final path = join(dataBasePath, "contacts.db");
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColuna INTEGER PRIMARY KEY, $nomeColuna TEXT, $emailColuna TEXT,$foneColuna TEXT,$imgColuna TEXT)");
+    });
+  }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+        columns: [idColuna, nomeColuna, emailColuna, foneColuna, imgColuna],
+        where: "$idColuna = ?",
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+}
 
 class Contact {
   int id;
@@ -20,15 +70,16 @@ class Contact {
     fone = map[foneColuna];
     img = map[imgColuna];
   }
-  Map toMap(){
-    Map<String,dynamic> map = {
-      nomeColuna : nome,
-      emailColuna : email,
-      foneColuna : fone,
-      imgColuna : img
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nomeColuna: nome,
+      emailColuna: email,
+      foneColuna: fone,
+      imgColuna: img
     };
-    if(id !=null){
-      map[idColuna]=id;
+    if (id != null) {
+      map[idColuna] = id;
     }
     return map;
   }
@@ -37,5 +88,4 @@ class Contact {
   String toString() {
     return "Contato(ID: $id, NOME: $nome, EMAIL: $email,FONE: $fone,IMG: $img)";
   }
-
 }
