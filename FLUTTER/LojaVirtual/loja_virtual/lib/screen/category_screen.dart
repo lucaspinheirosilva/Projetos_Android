19a05b031242b6417563992c852554dd0e16ce3e
Lucas Pinheiro <lucas_pinheiro.silva@hotmail.com>
2020-12-23 19:24:51 -0300

    Loja Virtual

--- FLUTTER/LojaVirtual/loja_virtual/lib/screen/category_screen.dart
+++ FLUTTER/LojaVirtual/loja_virtual/lib/screen/category_screen.dart
@@ -11,64 +11,65 @@ class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text(snapshot.data['title']),
-              centerTitle: true,
-              bottom: TabBar(
-                indicatorColor: Colors.white,
-                tabs: <Widget>[
-                  Tab(icon: Icon(Icons.grid_on)),
-                  Tab(
-                    icon: Icon(Icons.list),
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(snapshot.data['title']),
+          centerTitle: true,
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.grid_on)),
+              Tab(
+                icon: Icon(Icons.list),
+              )
+            ],
+          ),
+        ),
+        body: FutureBuilder<QuerySnapshot>(
+          future: Firestore.instance
+              .collection("products")
+              .document(snapshot.documentID)
+              .collection("items")
+              .getDocuments(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            else
+              return TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  GridView.builder(
+                      padding: EdgeInsets.all(4),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 4,
+                          crossAxisSpacing: 4,
+                          childAspectRatio: 0.65),
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) {
+                        return ProductTile(
+                            "grid",
+                            ProductData.fromDocument(
+                                snapshot.data.documents[index]));
+                      }),
+                  ListView.builder(
+                    padding: EdgeInsets.all(4),
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) {
+                      return ProductTile(
+                          "list",
+                          ProductData.fromDocument(
+                              snapshot.data.documents[index]));
+                    },
                   )
                 ],
-              ),
-            ),
-            body: FutureBuilder<QuerySnapshot>(
-                future: Firestore.instance
-                    .collection("products")
-                    .document(snapshot.documentID)
-                    .collection("items")
-                    .getDocuments(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData)
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  else
-                    return TabBarView(
-                      physics: NeverScrollableScrollPhysics(),
-                      children: [
-                        GridView.builder(
-                            padding: EdgeInsets.all(4),
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2,
-                                    mainAxisSpacing: 4,
-                                    crossAxisSpacing: 4,
-                                    childAspectRatio: 0.65),
-                            itemCount: snapshot.data.documents.length,
-                            itemBuilder: (context, index) {
-                              return ProductTile(
-                                  "grid",
-                                  ProductData.fromDocument(
-                                      snapshot.data.documents[index]));
-                            }),
-                        ListView.builder(
-                          padding: EdgeInsets.all(4),
-                          itemCount: snapshot.data.documents.length,
-                          itemBuilder: (context, index) {
-                            return ProductTile(
-                                "list",
-                                ProductData.fromDocument(
-                                    snapshot.data.documents[index]));
-                          },
-
-                        )
-                      ],
-                    );
-                })));
+              );
+          },
+        ),
+      ),
+    );
   }
 }
