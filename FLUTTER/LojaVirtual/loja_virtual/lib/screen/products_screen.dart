889ed3a8f037354d5eb60715cd02503181051297
Lucas Pinheiro <lucas_pinheiro.silva@hotmail.com>
2020-12-26 19:36:35 -0300

    Loja Virtual

--- FLUTTER/LojaVirtual/loja_virtual/lib/screen/products_screen.dart
+++ FLUTTER/LojaVirtual/loja_virtual/lib/screen/products_screen.dart
@@ -13,11 +13,14 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
+  String tam;
 
   _ProductScreenState(this.product);
 
   @override
   Widget build(BuildContext context) {
+    Color primaryColor = Theme.of(context).primaryColor;
+
     return Scaffold(
       //barra de titulo
       appBar: AppBar(
@@ -68,40 +71,75 @@ class _ProductScreenState extends State<ProductScreen> {
                   style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
                 ),
                 SizedBox(
-                  height: 34.0,
+                  height: 35.0,
                   child: GridView(
-                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    padding: EdgeInsets.symmetric(vertical: 4),
                     scrollDirection: Axis.horizontal,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 1,
-                        crossAxisSpacing: 8.0,
-                        childAspectRatio: 0.5),
+                        crossAxisSpacing: 0.8,
+                        childAspectRatio: 0.6),
                     children: product.tamanhos.map(
                       (tamanho) {
                         return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              tam = tamanho;
+                            });
+                          },
                           child: Container(
                             decoration: BoxDecoration(
                               borderRadius:
                                   BorderRadius.all(Radius.circular(5)),
-                              border:
-                                  Border.all(color: Colors.grey, width: 50.00),
+                              border: Border.all(
+                                  color: tam == tamanho
+                                      ? primaryColor
+                                      : Colors.grey,
+                                  width: 3),
+                              color: tam == tamanho
+                                  ? primaryColor
+                                  : Colors.transparent,
                             ),
-                            width: 50.0,
+                            width: 30.0,
                             alignment: Alignment.center,
-                            child: Text(tamanho),
+                            child: Text(tamanho,
+                                style: TextStyle(
+                                  color: tam == tamanho
+                                      ? Colors.white
+                                      : Colors.black,
+                                )),
                           ),
                         );
                       },
                     ).toList(),
                   ),
                 ),
-                Text("Descrição"),
+                SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                    onPressed: tam != null ? () {} : null,
+                    child: Text(
+                      "Adicionar ao carrino",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    color: primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text("Descrição",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
                 Text(
                   product.descricao,
                 ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
