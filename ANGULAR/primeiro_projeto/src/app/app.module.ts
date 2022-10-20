9a086e395b9a7384b7b84297975566a8a4f6514f
Lucas Pinheiro <lucas_pinheiro.silva@hotmail.com>
2022-10-19 23:54:39 -0300

    angular

--- ANGULAR/primeiro_projeto/src/app/app.module.ts
+++ ANGULAR/primeiro_projeto/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding/data-binding.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
