04fb66564b4975bbee219382170cc42ebbb5dbf0
Lucas Pinheiro <lucas_pinheiro.silva@hotmail.com>
2022-11-09 22:22:14 -0300

    angular

---
+++ ANGULAR/app-todo/src/app/modules/home/components/todo-imput-add-itens/todo-imput-add-itens.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-imput-add-itens',
+  templateUrl: './todo-imput-add-itens.component.html',
+  styleUrls: ['./todo-imput-add-itens.component.scss']
+})
+export class TodoImputAddItensComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ANGULAR/app-todo/src/app/modules/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Components
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoImputAddItensComponent } from './components/todo-imput-add-itens/todo-imput-add-itens.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoImputAddItensComponent,
+    TodoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
